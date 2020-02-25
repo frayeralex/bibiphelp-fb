8c30411df50d8562e95ab98f62571c6d67505d1c
Alex Boyko <frayeralex@gmail.com>
2020-02-25 12:26:30 +0200

    complete onEventCreate function

---
+++ functions/src/database/onEventCreate.ts
@@ -0,0 +1,12 @@
+import * as functions from 'firebase-functions';
+import { DataSnapshot } from "firebase-functions/lib/providers/database";
+
+export const onEventCreate = functions.database
+    .ref('/events/{eventId}')
+    .onCreate((snapshot: DataSnapshot, context: functions.EventContext) => {
+
+        return Promise.all([
+            snapshot.ref.child('userId').set(context.auth?.uid),
+            snapshot.ref.child('createdAt').set(Date.now())
+        ]);
+    });
