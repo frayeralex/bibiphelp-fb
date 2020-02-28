2712f9daad2c4b1947f2fccafe41dd51617017d5
Alex Boyko <frayeralex@gmail.com>
2020-02-28 08:31:33 +0200

    fix onUserFirstLogin.ts

--- functions/src/auth/onUserFirstLogin.ts
+++ functions/src/auth/onUserFirstLogin.ts
@@ -3,9 +3,7 @@ import * as admin from 'firebase-admin';
 import UserRecord = admin.auth.UserRecord;
 
 export const onUserFirstLogin = functions.auth.user().onCreate((user: UserRecord, context) => {
-    const appOptions = JSON.parse(process.env.FIREBASE_CONFIG!!);
-    appOptions.databaseAuthVariableOverride = context.auth;
-    const app = admin.initializeApp(appOptions, 'app');
+    const app = admin.initializeApp();
 
     const usersRef = app.database().ref('users/' + user.uid);
     const createdAt = usersRef.child('createdAt').set(Date.now());
